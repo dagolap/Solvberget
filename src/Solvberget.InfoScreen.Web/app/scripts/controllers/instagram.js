21148e57d80b440075373dcc18b3c23dc18d5b07
Fredrik Kalseth <fredrik@kalseth.com>
2014-04-23 15:17:59 +0200

    Parsing and showing Emojis on instagram captions

--- src/Solvberget.InfoScreen.Web/app/scripts/controllers/instagram.js
+++ src/Solvberget.InfoScreen.Web/app/scripts/controllers/instagram.js
@@ -3,8 +3,12 @@
 angular.module('solvbergetinfoScreenwebApp').controller('InstagramCtrl', function ($scope, $rootScope) {
 
     $rootScope.title = "Instagram feed";
-    
-    if($scope.template.slideOptions) $scope.tagName = $scope.template.slideOptions.tagName;
+
+    console.log($scope.template);
+    if ($scope.template.slideOptions) $scope.tagName = $scope.template.slideOptions.tagName;
     if (!$scope.tagName) $scope.tagName = 'sølvberget';
+
+    console.log("tag: " + $scope.tagName);
+
 });
 
